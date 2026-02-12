258fe1e376a4f0b0802fa951d3c11b8f19330f64
przxmus <46725495+przxmus@users.noreply.github.com>
2026-02-12 11:56:28 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { ComponentExample } from '@/components/component-example'
-
 export const Route = createFileRoute('/')({ component: App })
+
 
 function App() {
   return <></>
